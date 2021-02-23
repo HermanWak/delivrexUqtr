c896622e767361088b9d08071f6a59af7a84d031
herman <hermanlepro@gmail.com>
2021-02-22 21:25:53 -0500

    Initial Commit

---
+++ delivrexIOSUqtr/delivrexIOSUqtrApp.swift
@@ -0,0 +1,17 @@
+//
+//  delivrexIOSUqtrApp.swift
+//  delivrexIOSUqtr
+//
+//  Created by Herman Njomou Wakam on 2021-02-22.
+//
+
+import SwiftUI
+
+@main
+struct delivrexIOSUqtrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
